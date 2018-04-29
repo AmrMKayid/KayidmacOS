efd0ed91bd1bb4a1d25aaa01db223904d2c85231
Amr M. Kayid <amrkayid2027@gmail.com>
2018-04-29 16:43:42 +0200

    Add Xcode Dev Tools Installation

--- setup.sh
+++ setup.sh
@@ -14,3 +14,12 @@ print "#---------------------------------------------#\n\n"
 print "Root Access"
 sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+
+print "Xcode Dev Tools"
+if [ "$(pkgutil --pkg-info=com.apple.pkg.CLTools_Executables &> /dev/null)" ]; then
+    print 'Installing Xcode Dev Tools' ;
+    xcode-select --install
+else
+    print 'Xcode Dev Tools is already installed!';
+fi
