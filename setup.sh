cb01b96e154f9252e1eb2f8c006503cee603f6b6
Amr M. Kayid <amrkayid2027@gmail.com>
2018-04-29 16:29:07 +0200

    Ask for root password

--- setup.sh
+++ setup.sh
@@ -7,3 +7,10 @@ print() {
 print "\n#---------------------------------------------#"
 print "|          Kayid's Environment Setup          |"
 print "#---------------------------------------------#\n\n"
+
+
+# Root Access
+
+print "Root Access"
+sudo -v
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
